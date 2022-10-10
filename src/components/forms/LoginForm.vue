6facdf8f1f76f747cfd54490e0f2b94aaa644869
Boxer-boxer <jorgedasilvarodrigues92@gmail.com>
2022-10-10 15:59:21 -0400

    #40: Add validation message to login component

--- src/components/forms/LoginForm.vue
+++ src/components/forms/LoginForm.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
     const api_url = import.meta.env.VITE_API_URL
     const login = (event) => {
         const email = event.target.querySelector('[name="id-email-1"]').value
@@ -21,7 +21,7 @@
             console.log(errors)
         })
     }
-</script>
+</script> -->
 
 <template>
     <form
@@ -36,6 +36,8 @@
             label="Email"
             :required="true"
             :model="email"
+            @change="handleEmail"
+
         />
 
         <Button
@@ -52,6 +54,45 @@
 
     export default {
         components: {EmaiInput, Button, ValidationMessage},
+        data() {
+            return {
+                emailField: "",
+                message: "",
+                messageType: "",
+                api_url: import.meta.env.VITE_API_URL 
+            }
+        },
+        methods: {
+            handleEmail(v) {
+                this.emailField = v.target.value;
+            },
+            login() {
+                const payload = JSON.stringify({
+                    'email': this.subjectField,
+                })
+
+                fetch(this.api_url + '/magic-link', {
+                    headers: {
+                        'Content-Type': 'application/json;charset=UTF-8'
+                    },
+                    method: 'POST',
+                    body: payload
+                })
+                .then(response => {
+                    console.log(response)
+                    //TODO: notify user to check their email
+                    this.message = "Please check you e-mail to complete login."
+                    this.messageType = "success"
+                    window.location.href = '/'
+                })
+                .catch(errors => {
+                    console.log(errors)
+                    this.message = "An error has occured, please try again."
+                    this.messageType = "error"
+
+                })
+            }
+        }
     }
 </script>
 <style lang="scss">
