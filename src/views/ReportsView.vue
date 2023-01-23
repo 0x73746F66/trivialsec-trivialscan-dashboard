967e5760a4b1b78ce8522b3e2830e44d7a164a0d
chris <chris@trivialsec.com>
2023-01-23 14:57:28 +1100

    feat: show a config icon on summaries page

--- src/views/ReportsView.vue
+++ src/views/ReportsView.vue
@@ -11,13 +11,28 @@
             </div>
 
             <div class="w-100 margin-bottom-md">
-                <div class="d-flex flex-column margin-bottom-md">
-                    <h2 class="font-xl-sb">Report Summaries</h2>
-                    <span class="font-sm font-color-primary"
-                        >Trivial Scanner uploaded reports</span
+                <div class="d-flex margin-bottom-md">
+                    <div class="d-flex flex-column">
+                        <h2 class="font-xl-sb">Report Summaries</h2>
+                        <span class="font-sm font-color-primary"
+                            >Trivial Scanner uploaded reports</span
+                        >
+                    </div>
+                    <div
+                        class="d-flex flex-right"
+                        title="Scanner Configuration"
                     >
+                        <RouterLink
+                            to="/account/host-config"
+                            class="configure-icon font-color-light text-decoration-none"
+                        >
+                            <IconConfigure
+                                color="f0f0f0"
+                                class="router-link-icon"
+                            />
+                        </RouterLink>
+                    </div>
                 </div>
-
                 <Summary />
             </div>
         </div>
@@ -28,6 +43,7 @@
 import SearchForm from '@/components/forms/SearchForm.vue'
 import Summary from '@/components/general/Summary.vue'
 import DashboardMenu from '@/components/layout/DashboardMenu.vue'
+import IconConfigure from '@/components/icons/IconConfigure.vue'
 </script>
 
 <script>
@@ -35,7 +51,14 @@ export default {
     components: {
         SearchForm,
         Summary,
-        DashboardMenu
+        DashboardMenu,
+        IconConfigure
     }
 }
 </script>
+<style scoped>
+.configure-icon {
+    width: 32px;
+    height: 32px;
+}
+</style>
